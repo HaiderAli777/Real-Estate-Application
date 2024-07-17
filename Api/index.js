f359e07024ae13031462fd9bba4af11bd3872f2f
HaiderAli777 <ha7325897@gmail.com>
2024-07-17 07:15:17 -0700

    Connecting Database

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
 app.listen(3000, () => {
-  console.log("Hey from haider in port 3000");
+  console.log("Hey from haider in port 3000  ");
 });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
